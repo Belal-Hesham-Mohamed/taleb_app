33e57308a4be5388fe15b03aee6827a1b36bccc1
belal_h <compare12121@gmail.com>
2026-09-21 18:49:49 +0300

    update DioConfig to require SecureStorage for AuthInterceptor initialization

--- lib/core/networking/dio_config.dart
+++ lib/core/networking/dio_config.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:taleb/core/networking/interceptors/auth_interceptor.dart';
+import 'package:taleb/core/storage/secure_storage.dart';
 
 import 'api_config.dart';
 
 class DioConfig {
-  static Dio create(ApiConfig config) {
+  static Dio create({required ApiConfig config
+  ,    required SecureStorage secureStorage,
+}) {
     final dio = Dio(
       BaseOptions(
         baseUrl: config.baseUrl,
@@ -11,7 +15,14 @@ class DioConfig {
         receiveTimeout: config.receiveTimeout,
         sendTimeout: config.sendTimeout,
       ),
+      
     );
+    dio.interceptors.add(
+      AuthInterceptor(
+        secureStorage: secureStorage,
+      ),
+    );
+    
 
     return dio;
   }
